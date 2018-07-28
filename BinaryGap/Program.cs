659da5dd5bfb8e70fe38b765aab6fd90ae1e5c1b
korfu92 <konrad.korf@gmail.com>
2018-07-28 13:02:11 +0200

    first commit

--- BinaryGap/Program.cs
+++ BinaryGap/Program.cs
@@ -10,6 +10,16 @@ namespace BinaryGap
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This program caluclates maximum binary gap for a given number.");
+            Console.WriteLine("Please enter the number");
+
+            int intNumber = Int32.Parse(Console.ReadLine());
+            string binaryNumber = Convert.ToString(intNumber, 2);
+            Console.WriteLine($"Your number is {intNumber} and it's binary representation is {binaryNumber}. ");
+
+            var result = Solution.Gap(binaryNumber);
+            Console.WriteLine("Binary gap is: " +result);
+            Console.ReadLine();
         }
     }
 }
---
+++ BinaryGap/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryGap
+{
+    class Solution
+    {
+        public static int Gap(string binaryNumber)
+        {
+            int maxgap = 0;
+            int gap = 0;
+            for (var i = 0; i < binaryNumber.Length; i++)
+            {
+
+                if (binaryNumber[i] == '0')    
+                        { 
+                            gap++;
+                            
+                        } else
+                        {
+                        if (maxgap < gap)
+                            {
+                                maxgap = gap;
+                            }
+                        gap = 0;
+                        }
+                
+                //Console.WriteLine(binaryNumber[i]);
+            }
+            return maxgap;
+        }
+    }
+}
